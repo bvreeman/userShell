91b044c6e5e71c7d244a7e0ceec12dac5c1ce153
bvreeman <bvreeman@gmail.com>
2018-11-09 14:26:21 -0600

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+// const router = require("express").Router();
+// const apiRoutes = require("./api");
+// const contactFormRoutes = require("./contactForm");
+// const applicationFormRoutes = require("./applicationRoute")
+
+// // Routes
+// router.use("/api", apiRoutes);
+// router.use("/contactForm/send", contactFormRoutes)
+// router.use("/applicationRoute/send", applicationFormRoutes)
+
+// module.exports = router;
