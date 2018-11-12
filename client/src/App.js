27c1f4d41b975b1286650c61c70b6b97966d2ae3
bvreeman <bvreeman@gmail.com>
2018-11-12 15:14:19 -0600

    working on the nav just at a basic setup right now

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HeaderNav from './components/HeaderNav'
+
+
+import firebase from 'firebase/app';
+import 'firebase/database';
+// import admin from 'firebase-admin';
+
 
 class App extends Component {
   render() {
+
+    const config = {
+      apiKey: process.env.REACT_APP_APIKEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      databaseURL: process.env.REACT_APP_URL,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+    };
+  
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <HeaderNav />
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/HeaderNav/HeaderNav.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './HeaderNav.css';
+import {Link} from 'react-router-dom';
+
+const HeaderNav = () => 
+<div className="sticky headerContainer">
+    <nav className="navbar">
+        <div className="row navbar-header">
+            <div className='col-md-3 col-xs-12 navbarLeft'>
+                <Link to="/" className='navbarTitle'>Find A Consultant Now</Link>
+            </div>
+            <div className="col-md-9 col-xs-12 navbarCenter">
+                <Link to="/" className="navbar-brand">Home</Link>
+                {/* <Link to="/Community" className="navbar-brand">Community</Link> */}
+                <Link to="/About" className="navbar-brand">About</Link>
+                <Link to="/Gallery" className="navbar-brand">Gallery</Link>
+                <Link to="/DriversApplication" className="navbar-brand">Become a Driver</Link>
+                <Link to='/ContactUsPage' className="navbar-brand">Contact Us</Link>
+                <a className="navbarRight socialItems fb-ic ml-0" rel="noopener noreferrer" href="https://www.facebook.com/mnvalleytransport/" target="_blank" style={{color: '#ffffff'}}><i className="fa fa-facebook white-text mr-lg-4"></i></a>
+            </div>
+        </div>
+    </nav>
+</div>
+
+export default HeaderNav;
