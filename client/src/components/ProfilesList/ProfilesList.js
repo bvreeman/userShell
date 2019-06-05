b44c67788f582370f88332fbf25d1f098d001d6c
bvreeman <bvreeman@gmail.com>
2019-06-05 15:38:12 -0500

    working through randomizing the consultant profiles

--- client/src/components/ProfilesList/ProfilesList.js
+++ client/src/components/ProfilesList/ProfilesList.js
@@ -3,13 +3,35 @@ import ProfileSummary from '../ProfileSummary'
 import { Link } from 'react-router-dom'
 import './ProfilesList.css';
 
-const ProfilesList = ({users, auth}) => {
+// class ProfilesList extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             data: null
+//         }
+//     }
+
+//     componentWillMount() {
+//         this.loadData();
+//     }
+
+//     loadData = (users) => {
+//         console.log(this.props, 'users')
+//             // for (let i = users.length - 1; i > 0; i--) {
+//             //     const j = Math.floor(Math.random() * (i + 1));
+//             //     [users[i], users[j]] = [users[j], users[i]];
+//             // }
+//             // return users;
+//         }
+    const ProfilesList = ({users, auth}) => {
     // console.log(users, 'profile')
     // console.log(auth, 'auth in profile')
-
+    // render() {
+    //     console.log(this.props, 'props in render')
     return (
         <div className="project-list section">
             { users && users.map(profile => {
+                // console.log(Math.random(profile), 'profile')
                 return (
                     <Link to={'/businessProfile/' + profile.id} key={profile.id}>
                         <ProfileSummary profile={profile} />
@@ -19,5 +41,6 @@ const ProfilesList = ({users, auth}) => {
         </div>
     )
 }
+// }
 
 export default ProfilesList
