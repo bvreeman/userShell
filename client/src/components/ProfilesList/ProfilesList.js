3c3fea809fc5e12b8a3e7a921fa50d0a87f0dc95
bvreeman <bvreeman@gmail.com>
2019-06-10 13:18:26 -0500

    working through shuffle piece of existing consultants

--- client/src/components/ProfilesList/ProfilesList.js
+++ client/src/components/ProfilesList/ProfilesList.js
@@ -3,31 +3,62 @@ import ProfileSummary from '../ProfileSummary'
 import { Link } from 'react-router-dom'
 import './ProfilesList.css';
 
+
+// class ProfilesList extends React.PureComponent {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             newArray: [],
+//         }
+//     }
+// // const ProfilesList = ({users, auth}) => {
+//     // console.log(users, 'profile')
+//     // console.log(auth, 'auth in profile')
+//     render() { 
+//         let shuffleArray = async (array) => {
+//             let currentIndex = array.length, temporaryValue, randomIndex;
+          
+//             // While there remain elements to shuffle...
+//             while (0 !== currentIndex) {
+          
+//               // Pick a remaining element...
+//               randomIndex = Math.floor(Math.random() * currentIndex);
+//               currentIndex -= 1;
+          
+//               // And swap it with the current element.
+//               temporaryValue = array[currentIndex];
+//               array[currentIndex] = array[randomIndex];
+//               array[randomIndex] = temporaryValue;
+//             }
+//             return array;
+//           }
+//             shuffleArray(this.props.users).then(value => {
+//                 this.setState({ newArray: value })
+//             })
+//             console.log(this.state.newArray, 'newArray')
 const ProfilesList = ({users, auth}) => {
     // console.log(users, 'profile')
     // console.log(auth, 'auth in profile')
-    let shuffleArray = async (array) => {
-        console.log(array, 'array in shuffleArray')
-        let currentIndex = array.length, temporaryValue, randomIndex;
+    // let shuffleArray = async (array) => {
+    //     console.log(array, 'array in shuffleArray')
+    //     let currentIndex = array.length, temporaryValue, randomIndex;
       
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
+    //     // While there remain elements to shuffle...
+    //     while (0 !== currentIndex) {
       
-          // Pick a remaining element...
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex -= 1;
+    //       // Pick a remaining element...
+    //       randomIndex = Math.floor(Math.random() * currentIndex);
+    //       currentIndex -= 1;
       
-          // And swap it with the current element.
-          temporaryValue = array[currentIndex];
-          array[currentIndex] = array[randomIndex];
-          array[randomIndex] = temporaryValue;
-        }
-        return array;
-      }
-
-      shuffleArray(users)
+    //       // And swap it with the current element.
+    //       temporaryValue = array[currentIndex];
+    //       array[currentIndex] = array[randomIndex];
+    //       array[randomIndex] = temporaryValue;
+    //     }
+    //     return array;
+    //   }
+    //   const newArray = shuffleArray(users)
       
-      // Used like so
     return (
         <div className="project-list section">
             { users && users.map(profile => {
