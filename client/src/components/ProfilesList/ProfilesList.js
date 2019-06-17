c5c5227382f5e731c0427dc9a8fb904a8a1c30c3
bvreeman <bvreeman@gmail.com>
2019-06-16 22:36:32 -0500

    working through shuffle profile piece

--- client/src/components/ProfilesList/ProfilesList.js
+++ client/src/components/ProfilesList/ProfilesList.js
@@ -4,39 +4,51 @@ import { Link } from 'react-router-dom'
 import './ProfilesList.css';
 
 
-// class ProfilesList extends React.PureComponent {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             newArray: [],
-//         }
-//     }
-// // const ProfilesList = ({users, auth}) => {
-//     // console.log(users, 'profile')
-//     // console.log(auth, 'auth in profile')
-//     render() { 
-//         let shuffleArray = async (array) => {
-//             let currentIndex = array.length, temporaryValue, randomIndex;
+class ProfilesList extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newArray: [],
+            shuffledArray: [],
+        }
+    }
+
+    componentDidMount = async () => {
+         console.log(await this.props)
+    }
+
+// const ProfilesList = ({users, auth}) => {
+    // console.log(users, 'profile')
+    // console.log(auth, 'auth in profile')
+    render() { 
+        let shuffleArray = async (array) => {
+            await array;
+            console.log(array, 'array here?')
+            this.setState({ newArray: array})
+            console.log(this.state.newArray, 'just set state')
+            let currentIndex = await array.length, temporaryValue, randomIndex;
           
-//             // While there remain elements to shuffle...
-//             while (0 !== currentIndex) {
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
           
-//               // Pick a remaining element...
-//               randomIndex = Math.floor(Math.random() * currentIndex);
-//               currentIndex -= 1;
+              // Pick a remaining element...
+              randomIndex = Math.floor(Math.random() * currentIndex);
+              currentIndex -= 1;
           
-//               // And swap it with the current element.
-//               temporaryValue = array[currentIndex];
-//               array[currentIndex] = array[randomIndex];
-//               array[randomIndex] = temporaryValue;
-//             }
-//             return array;
-//           }
-//             shuffleArray(this.props.users).then(value => {
-//                 this.setState({ newArray: value })
-//             })
-//             console.log(this.state.newArray, 'newArray')
-const ProfilesList = ({users, auth}) => {
+              // And swap it with the current element.
+              temporaryValue = this.state.newArray[currentIndex];
+              this.state.newArray[currentIndex] = this.state.newArray[randomIndex];
+              this.state.newArray[randomIndex] = temporaryValue;
+            }
+            return this.state.newArray;
+          }
+        shuffleArray(this.state.newArray).then(value => {
+            console.log(value, 'value inside shuffle')
+            this.setState({ shuffledArray: value })
+            console.log(this.state.shuffledArray, 'newArray after setstate')
+        })
+        console.log(this.state.shuffledArray, 'newArray')
+// const ProfilesList = ({users, auth}) => {
     // console.log(users, 'profile')
     // console.log(auth, 'auth in profile')
     // let shuffleArray = async (array) => {
@@ -58,11 +70,12 @@ const ProfilesList = ({users, auth}) => {
     //     return array;
     //   }
     //   const newArray = shuffleArray(users)
+    //   console.log(newArray, 'newArray')
       
     return (
         <div className="project-list section">
-            {console.log(users, 'users in return')}
-            { users && users.map(profile => {
+            {console.log(this.state.newArray, 'users in return')}
+            { this.state.shuffledArray && this.state.shuffledArray.map(profile => {
                 return (
                     <Link to={'/businessProfile/' + profile.id} key={profile.id}>
                         <ProfileSummary profile={profile} />
@@ -71,6 +84,7 @@ const ProfilesList = ({users, auth}) => {
             })}
         </div>
     )
+}
 }
 
 export default ProfilesList
